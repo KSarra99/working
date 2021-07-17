a5bbd085a422d4a69c576401a3ea267e59c597bb
Sarra <Khmilasarra@gmail.com>
2021-07-17 16:38:44 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import "./style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <>
+  <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+    <h1 className="title-red">Khmila Sarra</h1>
+    <br />
+    <img src="/imageInSrc.jpg" alt="me"/>
+    <br />
+    <img src="/imageInPublic.jpg" alt="me"/>
+  </div>
+</>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
